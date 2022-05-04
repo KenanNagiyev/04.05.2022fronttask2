013d64ac3963572c4288a18d7f7b11f3a89520b6
KenanNagiyev <99851049+KenanNagiyev@users.noreply.github.com>
2022-05-04 11:02:45 +0400

    04.05.2022fronttask2

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function openNav() {
+    document.getElementById("SidebarMenu").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("SidebarMenu").style.width = "0";
+  }
